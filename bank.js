5f9216371583eb6efd534ee0189fdfdbde3532c7
Kareem Hisham <safariknight2@gmail.com>
2025-02-15 15:07:04 +0200

    feat: handled error

--- bank.js
+++ bank.js
@@ -4,8 +4,29 @@ function bankAccount(name, age, balance, address) {
     age: age,
     balance: balance,
     address: address,
-    accountInfo: function() {
-      console.log(`Name: ${this.name}`)
-    }
-  }
+    accountInfo: function () {
+      console.log(`Name: ${this.name}`);
+    },
+    deposit: function (amount) {
+      return new Promise((resolve, reject) => {
+        if (amount <= 0) {
+          reject("Invalid amount");
+        }
+        setTimeout(() => {
+          this.balance += amount;
+          resolve(amount);
+        }, 1000);
+      });
+    },
+    addAmount: function (amount) {
+      this.deposit(amount)
+        .then((amount) => {
+          console.log(`Amount Deposited: ${amount}`);
+          console.log(`New Balance: ${this.balance}`);
+        })
+        .catch((message) => {
+          console.log(message);
+        });
+    },
+  };
 }
