b177241ad076f76a832b13235100d3f805ef105e
Kareem Hisham <safariknight2@gmail.com>
2025-02-15 15:20:49 +0200

    feat: deposit

--- bank.js
+++ bank.js
@@ -6,6 +6,27 @@ function bankAccount(name, age, balance, address) {
     address: address,
     accountInfo: function() {
       console.log(`Name: ${this.name}`)
-    }
+    },
+    deposit: function (amount) {
+      return new Promise((resolve, reject) => {
+        if (amount <= 0) {
+          reject("Invalid amount");
+        }
+        setTimeout(() => {
+          this.balance += amount;
+          resolve(amount);
+        }, 1000);
+      });
+    },
+    addAmount: function (amount) {
+      this.deposit(amount)
+        .then((amount) => {
+          console.log(`Amount Deposited: ${amount}`);
+          console.log(`New Balance: ${this.balance}`);
+        })
+        .catch((message) => {
+          console.log(message);
+        });
+    },
   }
 }
