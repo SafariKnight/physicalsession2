147f73e4357f52b1272b8c2c55a2acd59fee7d0f
Pero <pierretameradel@gmail.com>
2025-02-15 15:42:32 +0200

    the code

--- bank.js
+++ bank.js
@@ -7,27 +7,28 @@ function bankAccount(name, age, balance, address) {
     accountInfo: function () {
       console.log(`Name: ${this.name}`);
     },
-    balanceCompare: function (amount) {
-      console.log("Checking balance...");
-      return new Promise((resolve, reject) => {
+    balanceCompare: async function (amount) {
+      console.log("Checking balance..."),
         setTimeout(() => {
           if (amount <= 0) {
-            reject(new Error("Invalid Withdraw Amount"));
+            throw new Error("Invalid Withdraw Amount");
           }
-          if (this.balance < amount) {
-            reject(new Error("Withdraw Amount Higher than Balance"));
+          if (this.balance <= amount) {
+            throw new Error("Withdraw Amount Higher than Balance");
           }
-          resolve(true);
+          return true;
         }, 1000);
-      });
     },
     withdraw: async function (money) {
       try {
-        await this.balanceCompare(money);
-        this.balance -= money;
-        console.log(`Withdrawal successful. New Balance: ${this.balance}`);
+        if (this.balanceCompare(money)) {
+          this.balance -= money;
+          console.log(`Withdrawal successful. New Balance: ${this.balance}`);
+        } else {
+          console.log("Invalid withdrawal amount or insufficient balance.");
+        }
       } catch (err) {
-        console.log(err.message);
+        console.log(err.message)
       }
     },
     deposit: function (amount) {
@@ -51,5 +52,5 @@ function bankAccount(name, age, balance, address) {
           console.log(message);
         });
     },
-  };
+  }
 }
